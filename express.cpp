750f8ab52069657b98e39b7f73ab2bb2849392a4
v07-v <vedantk8877@gmail.com>
2025-05-07 09:31:26 +0530

    Update express.cpp

--- express.cpp
+++ express.cpp
@@ -1,223 +1,136 @@
 #include <iostream>
 #include <string.h>
-#include <stack>
 using namespace std;
 
-class node
-{
+class Node {
 public:
-     char data;
-     node *left;
-     node *right;
+    char data;
+    Node *left, *right;
 };
 
-class tree
-{
-public:
-     node *root;
-     void expression(char[]);
-     void display(node *);
-     void inorder(node *);
-     void preorder(node *);
-     void postorder(node *);
-     void remove(node *);
-     void non_rec_postorder(node *);
-};
-
-class stack1
-{
-     node *data[30];
-     int top;
+class Stack {
+    Node *data[30];
+    int top;
 
 public:
-     stack1()
-     {
-          top = -1;
-     }
-     int empty()
-     {
-          if (top == -1)
-               return 1;
-          return 0;
-     }
-     void push(node *p)
-     {
-          data[++top] = p;
-     }
-     node *pop()
-     {
-          return (data[top--]);
-     }
+    Stack() { top = -1; }
+
+    bool empty() { return top == -1; }
+
+    void push(Node *p) { data[++top] = p; }
+
+    Node *pop() { return data[top--]; }
 };
 
-void tree::expression(char prefix[])
-{
-     stack1 s;
-     int len;
-     node *t1, *t2;
-     len = strlen(prefix);
-     for (int i = len - 1; i >= 0; i--)
-     {
-          root = new node;
-          root->left = NULL;
-          root->right = NULL;
-          if (isalpha(prefix[i]))
-          {
-               root->data = prefix[i];
-               s.push(root);
-          }
-          else if (prefix[i] == '+' || prefix[i] == '-' || prefix[i] == '*' || prefix[i] == '/')
-          {
-               root->data = prefix[i];
-               t2 = s.pop();
-               t1 = s.pop();
-               root->left = t2;
-               root->right = t1;
-               s.push(root);
-          }
-     }
-     root = s.pop();
-}
+class Tree {
+    Node *root;
 
-void tree::inorder(node *temp)
-{
-     if (temp != NULL)
-     {
-          inorder(temp->left);
-          cout << temp->data << " ";
-          inorder(temp->right);
-     }
-}
+public:
+    Tree() { root = nullptr; }
 
-void tree::postorder(node *temp)
-{
-     if (temp != NULL)
-     {
-          postorder(temp->left);
-          postorder(temp->right);
-          cout << temp->data << " ";
-     }
-}
+    void buildFromPrefix(char prefix[]) {
+        Stack s;
+        int len = strlen(prefix);
+        for (int i = len - 1; i >= 0; i--) {
+            Node *newNode = new Node;
+            newNode->left = newNode->right = nullptr;
+            newNode->data = prefix[i];
 
-void tree::non_rec_postorder(node *top)
-{
-     stack1 s1, s2;
-     node *t = top;
-     cout << "\n";
-     s1.push(t);
-     while (!s1.empty())
-     {
-          t = s1.pop();
-          s2.push(t);
-          if (t->left != NULL)
-          {
-               s1.push(t->left);
-          }
-          if (t->right != NULL)
-          {
-               s1.push(t->right);
-          }
-     }
-     while (!s2.empty())
-     {
-          top = s2.pop();
-          cout << top->data;
-     }
-}
+            if (isalpha(prefix[i])) {
+                s.push(newNode);
+            } else {
+                // FIX: Correct child assignment
+                Node* right = s.pop();
+                Node* left = s.pop();
+                newNode->left = left;
+                newNode->right = right;
+                s.push(newNode);
+            }
+        }
+        root = s.pop();
+        cout << "\nExpression tree built successfully.\n";
+    }
 
-void tree::preorder(node *temp)
-{
-     if (temp != NULL)
-     {
-          cout << temp->data << " ";
-          preorder(temp->left);
-          preorder(temp->right);
-     }
-}
+    void nonRecursivePostOrder() {
+        if (!root) {
+            cout << "\nTree is empty.\n";
+            return;
+        }
 
-void tree::remove(node *temp)
-{
-     if (temp == NULL)
-     {
-          return;
-     }
-     remove(temp->left);
-     remove(temp->right);
-     cout << "\nDeleting Node : " << temp->data << endl;
-     delete temp;
-}
+        Stack s1, s2;
+        s1.push(root);
 
-void tree::display(node *temp)
-{
-     if (temp != NULL)
-     {
-          cout << temp->data;
-          display(temp->left);
-          display(temp->right);
-     }
-}
+        while (!s1.empty()) {
+            Node *temp = s1.pop();
+            s2.push(temp);
 
-int main()
-{
-     tree t1;
-     int ch;
-     do
-     {
-          cout << "\nMENU";
-          cout << "\n1. Expression ";
-          cout << "\n2. Display";
-          cout << "\n3. Display Inorder";
-          cout << "\n4. Display Postorder";
-          cout << "\n5. Display Preorder";
-          cout << "\n6. Display Non-Recursive Postorder";
-          cout << "\n7. Delete Node";
-          cout << "\n8. Exit";
-          cout << "\nEnter your choice: ";
-          cin >> ch;
+            if (temp->left) s1.push(temp->left);
+            if (temp->right) s1.push(temp->right);
+        }
 
-          cout << "You entered: " << ch << endl;
+        cout << "\nPost-order traversal (non-recursive): ";
+        while (!s2.empty()) {
+            cout << s2.pop()->data << " ";
+        }
+        cout << endl;
+    }
 
-          switch (ch)
-          {
-          case 1:
-               char expr[30];
-               cout << "\nEnter Expression: ";
-               cin >> expr;
-               t1.expression(expr);
-               break;
-          case 2:
-               cout << "\nExpression : ";
-               t1.display(t1.root);
-               break;
-          case 3:
-               cout << "Inorder : ";
-               t1.inorder(t1.root);
-               break;
-          case 4:
-               cout << "Postorder : ";
-               t1.postorder(t1.root);
-               break;
-          case 5:
-               cout << "Preorder : ";
-               t1.preorder(t1.root);
-               break;
-          case 6:
-               cout << "Non-Recursive Postorder : ";
-               t1.non_rec_postorder(t1.root);
-               break;
-          case 7:
-               t1.remove(t1.root);
-               break;
-          case 8:
-               cout << "\nThe End!!!";
-               cout << "\nThank You for using the Program !!!!";
-               break;
-          default:
-               cout << "Invalid choice!!" << endl;
-          }
+    void deleteTree(Node *temp) {
+        if (!temp) return;
+        deleteTree(temp->left);
+        deleteTree(temp->right);
+        cout << "Deleting Node: " << temp->data << endl;
+        delete temp;
+    }
 
-          cout << "End of choice: " << ch << endl;
+    void deleteEntireTree() {
+        if (!root) {
+            cout << "\nTree is already empty.\n";
+            return;
+        }
+        deleteTree(root);
+        root = nullptr;
+        cout << "\nExpression tree deleted successfully.\n";
+    }
 
-     } while (ch != 8);
-     return 0;
+    ~Tree() {
+        deleteEntireTree();
+    }
+};
+
+int main() {
+    Tree t;
+    int choice;
+    char expr[30];
+
+    do {
+        cout << "\n--- MENU ---\n";
+        cout << "1. Build Expression Tree from Prefix\n";
+        cout << "2. Post-order Traversal (Non-Recursive)\n";
+        cout << "3. Delete Expression Tree\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            cout << "\nEnter prefix expression: ";
+            cin >> expr;
+            t.buildFromPrefix(expr);
+            break;
+        case 2:
+            t.nonRecursivePostOrder();
+            break;
+        case 3:
+            t.deleteEntireTree();
+            break;
+        case 4:
+            cout << "\nExiting program.\n";
+            break;
+        default:
+            cout << "Invalid choice. Try again.\n";
+        }
+    } while (choice != 4);
+
+    return 0;
 }
